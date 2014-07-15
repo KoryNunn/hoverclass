2f2c7a341b3a29da5bac9a9d7b3639bc37527ca8
KoryNunn <knunn187@gmail.com>
2014-07-15 11:17:50 +1000

    Corrected target element

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ module.exports = function(hoverClass, selector){
         hoverClass = hoverClass || 'hover';
 
     doc(document).on('touchstart mousedown', selector, function(event){
-        doc(event.target).addClass(hoverClass);
-        down.push(event.target);
+        var target = doc(event.target).closest(selector);
+        target.addClass(hoverClass);
+        down.push(target);
     });
     doc(document).on('touchend mouseup', function(event){
         while(down.length){
-            doc(down.pop()).removeClass(hoverClass);
+            down.pop().removeClass(hoverClass);
         }
     });
 };
